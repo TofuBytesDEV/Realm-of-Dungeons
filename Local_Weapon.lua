8f5a090d64baf3de0e14dd723367c44ce2b45973
wall142 <robloxnoblox@gmail.com>
2016-11-30 16:38:24 -0800

    Update Local_Weapon.lua

--- Local_Weapon.lua
+++ Local_Weapon.lua
@@ -3,18 +3,20 @@ script.Parent.Equipped:connect(function(mouse)
 	mouse.Button2Down:connect(function()
 		script.Parent.IsBlocking.Value = true
 		script.Parent.Block:FireServer()
-	end
+	end)
 	mouse.Button2Up:connect(function()
 		script.Parent.IsBlocking.Value = false
-	end
+	end)
 	--Gamepad Detection
-  game:GetService('UserInputService').InputBegan:connect(function(input)
-    if input.KeyCode == Enum.KeyCode.ButtonL2 then
-      script.Parent.IsBlocking.Value = true
-      script.Parent.block:FireServer()
-    end
-  game:GetService('UserInputService').InputEnded:connect(function(input)
-    if input.KeyCode == Enum.KeyCode.ButtonL2 then
-      script.Parent.IsBlocking.Value = false
-    end
+	game:GetService('UserInputService').InputBegan:connect(function(input)
+		if input.KeyCode == Enum.KeyCode.ButtonL2 then
+		script.Parent.IsBlocking.Value = true
+		script.Parent.block:FireServer()
+		end
+	end)
+	game:GetService('UserInputService').InputEnded:connect(function(input)
+		if input.KeyCode == Enum.KeyCode.ButtonL2 then
+		script.Parent.IsBlocking.Value = false
+		end
+	end)
 end)
